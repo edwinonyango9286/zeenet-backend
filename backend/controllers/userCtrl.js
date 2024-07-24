e5b9a4b38e579204463b8e335e5936a4ed708749
edwin onyango <ookoedwin29@gmail.com>
2024-07-24 16:50:28 +0300

    commit

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -271,7 +271,7 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
   try {
     const token = await user.createPasswordResetToken();
     await user.save();
-    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid 10 minutes from now. <a href='http://localhost:3000/reset-password/${token}'>Click Here</>`;
+    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid 10 minutes from now. <a href='https://zeenet-frontstore.onrender.com/reset-password/${token}'>Click Here</>`;
     const data = {
       to: email,
       text: "Zeenet App Developers",
